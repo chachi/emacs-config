60eb281c3307143f116d8cc67732cfc9135eec29
Jack Morrison <jackmorrison1@gmail.com>
2014-05-02 10:00:24 -0400

    Colorize compilation buffer.

--- my-colors.el
+++ my-colors.el
@@ -12,6 +12,13 @@
 ;;   (global-hl-line-mode t))
 ;; turn it on for all modes by default
 
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
 (provide 'my-colors)
 ;;; my-colors.el ends here
 
